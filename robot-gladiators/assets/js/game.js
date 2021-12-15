4e2d7abdbd88430e8566ce6a5ccc81370970c9e2
Besupan <unsunade@hotmail.com>
2021-12-15 04:04:49 -0600

    add enemy combatants

--- robot-gladiators/assets/js/game.js
+++ robot-gladiators/assets/js/game.js
@@ -4,38 +4,41 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Bender";
+var enemyNames = ["Bender", "Flexo", "Calculon", "The Robot Devil"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames.length);
+
+var fight = function(enemyName) {
     //Round starting
     window.alert("Welcome to Robot Gladiators!");
 };
 
-console.log(enemyName + "'s health is now at " + enemyHealth);
+console.log(enemyNames + "'s health is now at " + enemyHealth);
 
 console.log(playerName + "'s health is now at " + playerHealth);
 
-fight();
+fight(enemyNames);
+
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 console.log(promptFight);
 
 if(promptFight === "fight" || promptFight === "FIGHT"){
     enemyHealth = (enemyHealth - playerAttack);
-    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    console.log(playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining.");
 
     //check enemy's health
 if(enemyHealth <= 0) {
-    window.alert(enemyName + "has died!");
+    window.alert(enemyNames + "has died!");
 }
 else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    window.alert(enemyNames + " still has " + enemyHealth + " health left.");
 }
 
 playerHealth = (playerHealth - enemyAttack);
-console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+console.log(enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
 if(playerHealth < 0) {
     window.alert(playerName + " got dedded.");
@@ -49,25 +52,30 @@ else {
     var confirmSkip = window.confirm("Are you sure you'd like to run away?");
 
     //if yes (true), leave fight
-if(confirmSkip) {
-    window.alert(playerName + " has chickened out!");
-    // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 2;
-    console.log(playerName + " has " + playerMoney + " pieces of gold left.");
+    if(confirmSkip) {
+        window.alert(playerName + " has chickened out!");
+        // subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+        console.log(playerName + " has " + playerMoney + " gold left.")
     }
-//if no (false), ask question again by running fight() again
-else{
-    fight();
- }
+    //if no (false), ask question again by running fight() again
+    else{
+        fight();
+    }
+    
+} else {
+    window.alert("Type 'SKIP' or 'FIGHT'. Redo.");
 }
-
-
 
 //check to see if the value of the playerHealth variable is greater than 0
 if(playerHealth > 0) {
     console.log("Your player is still alive!");
 }
 
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
 
 
