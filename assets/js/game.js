10e4fc7678dae390b29e28fe22ccd4a967260269
Besupan <unsunade@hotmail.com>
2022-01-02 15:19:29 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,13 +35,14 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + " chickened out!");
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(0, playerMoney - 10);
       console.log("gold = ", playerMoney);
       break;
     }
   } 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
@@ -58,7 +59,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
@@ -75,6 +77,12 @@ var fight = function(enemyName) {
 
 };
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return value;
+}
+
 var startGame = function() {
   //reset player stats
   var playerHealth = 100;
@@ -88,7 +96,7 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert("Round " + (i +1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
       //if wer're not at the last enemy in the array
       if (playerHealth > 0 && i < enemyNames.length - 1) {
